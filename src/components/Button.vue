48d4c6d5bb0da136733d9d81ffdca8290a690799
RoyMontoya <roy_a18@hotmail.com>
2020-04-19 18:03:16 -0500

    made button and counter work

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-on:click="clicked">Click</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  methods:{
+    clicked() {
+        this.$emit('clicked');
+    }
+  }
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ count }}</h1>
+    <Button @clicked="count++" />
+  </div>
+</template>
+
+<script>
+
+import Button from './Button'
+
+export default {
+  name: 'Counter',
+  components: {
+    Button
+  },
+  data: function() {
+    return {
+      count: 0
+      }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
